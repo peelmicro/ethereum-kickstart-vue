e18d6c528214df21e212ab5874e59e6b434235dc
juanp_perez@loyaltycrm.com <juanp_perez@loyaltycrm.com>
2018-03-31 10:13:10 +0200

    Setting up Nuxt

---
+++ plugins/vuetify.js
@@ -0,0 +1,14 @@
+import Vue from 'vue'
+import Vuetify from 'vuetify'
+import colors from 'vuetify/es5/util/colors'
+Vue.use(Vuetify, {
+  theme: {
+    primary: colors.red.darken2,
+    accent: colors.red.accent2,
+    secondary: colors.grey.lighten1,
+    info: colors.blue.lighten1,
+    warning: colors.amber.darken2,
+    error: colors.red.accent4,
+    success: colors.green.lighten2
+  }
+})
