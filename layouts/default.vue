5d3c3367f63887958a3a52d5813f0de96d369044
juanp_perez@loyaltycrm.com <juanp_perez@loyaltycrm.com>
2018-04-01 09:42:09 +0200

    Initial layout set up

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,12 +1,20 @@
 <template>
   <v-app>
     <v-navigation-drawer app></v-navigation-drawer>
-    <v-toolbar app></v-toolbar>
+    <v-toolbar app>
+      <v-toolbar-title>
+        Im the header
+      </v-toolbar-title>
+    </v-toolbar>
     <v-content>
       <v-container fluid>
         <router-view></router-view>
       </v-container>
     </v-content>
-    <v-footer app></v-footer>
+    <v-footer app>
+      <v-layout row>
+        <v-flex xs12>Im the footer</v-flex>
+      </v-layout>
+    </v-footer>
   </v-app>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,52 @@
 <template>
-  <h1>First Campaign: {{ campaigns[0] }}</h1>
+  <v-container>
+    <v-layout row>
+      <v-flex xs12>
+        Open Campaigns
+      </v-flex>
+    </v-layout>
+    <v-layout 
+      row 
+      wrap
+      v-for="campaign in campaigns"
+      :key="campaign.id"     
+      class="mb-2" 
+      >
+      <v-flex xs12 md8>
+        <v-card class="info">
+            <v-layout row>
+              <v-flex xs12>
+                <v-card-title primary-title>
+                  <div>
+                    <h5>{{campaign.id}}</h5>
+                  </div>
+                </v-card-title>
+                <v-card-actions>
+                  <v-btn flat>
+                    <v-icon left light>arrow_forward</v-icon>
+                    View Campaign
+                  </v-btn>
+                </v-card-actions>
+              </v-flex>              
+            </v-layout>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
 import factory from '../ethereum/factory'
 export default {
   async asyncData () {
-    const campaigns = await factory.methods.getDeployedCampaigns().call()
+    let campaigns = await factory.methods.getDeployedCampaigns().call()
+    campaigns = campaigns.length
+      ? campaigns.map(address => {
+        return {
+          id: address
+        }
+      })
+      : {}
     console.log(campaigns)
     return {
       campaigns
